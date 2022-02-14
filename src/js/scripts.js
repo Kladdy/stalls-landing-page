c2ed92048a5447ad0fa5138978b8cc8371fd73c4
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-02-14 16:14:56 +0100

    Update lang and navbar

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,9 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+$(function(){
+    $('a').each(function(){
+        if ($(this).prop('href') == window.location.href) {
+            $(this).addClass('active'); $(this).parents('li').addClass('active');
+        }
+    });
+});
