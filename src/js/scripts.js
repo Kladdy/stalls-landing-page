7fd8f79c3c0ab925d8fff5522bde8ca2d9a7daf0
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-02-14 20:33:57 +0100

    Update styling and home page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,9 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// Can also use  $() instead of $(document).ready()
+$(document).ready(function () {
+    $('div.title-to-fade-in').fadeIn(1000).removeClass('title-to-fade-in');
+    $('div.subtitle-to-fade-in').fadeIn(3000).removeClass('subtitle-to-fade-in');
+
+});
