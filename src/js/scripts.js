d863075bd1b8ccfbee196383cc52853252b7df82
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-02-14 16:40:34 +0100

    Begind the use of extends and layout

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,2 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-$(function(){
-    $('a').each(function(){
-        if ($(this).prop('href') == window.location.href) {
-            $(this).addClass('active'); $(this).parents('li').addClass('active');
-        }
-    });
-});
