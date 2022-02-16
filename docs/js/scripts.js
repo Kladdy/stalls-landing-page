fdf2810b3f6d39a34f79f71589383b636f9db8b8
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-02-16 13:48:21 +0100

    Publish first draft

---
+++ docs/js/scripts.js
@@ -0,0 +1,14 @@
+/*!
+* Start Bootstrap - Landing Page v6.0.4 (https://startbootstrap.com/theme/landing-page)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// Can also use  $() instead of $(document).ready()
+$(document).ready(function () {
+    $('div.title-to-fade-in').fadeIn(1000).removeClass('title-to-fade-in');
+    $('div.subtitle-to-fade-in').fadeIn(3000).removeClass('subtitle-to-fade-in');
+
+});
