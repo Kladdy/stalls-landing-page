98e87da79dfbfaa582b9a0b01659da47e22be1a7
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-03-01 07:35:56 +0100

    Update navbar javascript to ignore query params

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,6 +1,9 @@
+// Mark the navbar option that is selected
+// Use window.location.href.split('?')[0] to make sure that
+// we dont take in consideration any query parameters
 $(function(){
     $('a').each(function(){
-        if ($(this).prop('href') == window.location.href) {
+        if ($(this).prop('href') == window.location.href.split('?')[0]) {
             $(this).addClass('active'); $(this).parents('li').addClass('active');
         }
     });
